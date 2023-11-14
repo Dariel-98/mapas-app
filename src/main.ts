571e34c31638b85e4c412a96a33d82e59a9cf93d
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-14 17:40:53 -0600

    Show Mapbox map

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken =
+  'pk.eyJ1IjoiZGFyaWVsLTk4IiwiYSI6ImNsb3l2MjJhbjA3bWQyam1zaXQybHczbHcifQ.1hMRESvsXsXUux3BWqFO6A';
+
 if (!navigator.geolocation) {
   alert('Navegador no soporta la Geolocation');
   throw new Error('Navegador no soporta la Geolocation');
