a8a516c3ba37bf1d4cd8b4d637a7f7f23a59eb36
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-14 14:32:48 -0600

    Show loading message

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
 
 @Component({
@@ -6,7 +6,10 @@ import { PlacesService } from '../../services/places.service';
   templateUrl: './map-screen.component.html',
   styleUrls: ['./map-screen.component.css'],
 })
-export class MapScreenComponent implements OnInit {
+export class MapScreenComponent {
   constructor(private placesService: PlacesService) {}
-  ngOnInit(): void {}
+
+  get isUserLocationReady() {
+    return this.placesService.isUserLocationReady;
+  }
 }
