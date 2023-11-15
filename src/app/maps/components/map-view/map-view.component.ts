f95702492842358d2751387cedc04b044c63ea4a
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-14 22:03:34 -0600

    Map service

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
 import { Map, Marker, Popup } from 'mapbox-gl';
+import { MapService } from '../../services/map.service';
 
 @Component({
   selector: 'app-map-view',
@@ -11,7 +12,10 @@ export class MapViewComponent implements AfterViewInit {
   @ViewChild('mapDiv')
   mapDivElement!: ElementRef;
 
-  constructor(private placesService: PlacesService) {}
+  constructor(
+    private placesService: PlacesService,
+    private mapService: MapService
+  ) {}
 
   ngAfterViewInit(): void {
     if (!this.placesService.userLocation)
@@ -32,5 +36,7 @@ export class MapViewComponent implements AfterViewInit {
       .setLngLat(this.placesService.userLocation)
       .setPopup(popup)
       .addTo(map);
+
+    this.mapService.setMap(map);
   }
 }
