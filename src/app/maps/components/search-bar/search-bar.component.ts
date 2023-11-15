e695cc40fba0055c844a771a49d5f6a3a3beef83
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-14 22:27:35 -0600

    Manual Debounce

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  private debounceTimer?: NodeJS.Timeout;
 
+  onQueryChanged(query: string = '') {
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+      console.log('Mandar este query:', query);
+    }, 350);
+  }
 }
